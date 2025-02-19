95535cf0750874631f338620ff07ee973de6ae32
jamestech.ke <logomakingpro21@gmail.com>
2025-02-19 12:04:18 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -57,25 +57,22 @@ router.get('/', async (req, res) => {
                let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data });
 
                let GIFTED_MD_TEXT = `
-*_Pair Code Connected by Gifted Tech_*
-*_Made With 🤍_*
+*_COPY ABOVE SESSION FOR JAMES MD BOT🌹_*
 ______________________________________
 ╔════◇
-║ *『 WOW YOU CHOOSEN GIFTED-MDv2 』*
-║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
-╚══════════════════════╝
+║ *『 💞THIS IS JAMES MD💞』*
+║ _YOU CAN NOW DEPLOY._
+╚════════════════════╝
 ╔═════◇
-║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Ytube:* _youtube.com/@giftedtechnexus_
-║❒ *Owner:* _https://wa.me/message/NHCZC5DSOEUXB1_
-║❒ *Repo:* _https://github.com/Giftedmaurice/gifted-mdV2_
-║❒ *WaGroup:* _https://chat.whatsapp.com/L0ctUUVLlsrFYwBHApKfew_
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaJmfmTDJ6H7CmuBss0o_
-║❒ *Plugins:* _https://github.com/Giftedmaurice/gifted-bot-md-plugins_
+║  『••• 🌹INFO🌹 •••』
+║❒ *Owner:* _JAMES
+║❒ *WaGroup:* _https://chat.whatsapp.com/F0x0O4o8reS15D6kVfAChW_
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaogSY74IBhJWe8b472H_
+║❒ *repo:* _https://github.com/jtechde/james_
 ╚══════════════════════╝ 
 _____________________________________
 
-_Don't Forget To Give Star To My Repo_`
+_Don't Forget To Give Star To My Repo🥵_`
  await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id,{text:GIFTED_MD_TEXT},{quoted:session})
  
 
